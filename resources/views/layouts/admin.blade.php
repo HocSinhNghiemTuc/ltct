1ba537480717f0801a51382eb2857ef6b24542ae
Hoang Hieu <hieu.jno1@gmail.com>
2020-11-30 14:58:36 +0700

    change js + css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,10 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>AdminLTE 3 | Starter</title>
-
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{asset('AdminLTE-3.0.5/dist/css/adminlte.min.css')}}">
-    <link rel="stylesheet" href="{{asset('modules/contact/css/app.css')}}">
+    @yield('css')
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body class="hold-transition sidebar-mini">
@@ -23,6 +22,6 @@
 </div>
 <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 <script type="text/javascript" src="{{asset('AdminLTE-3.0.5/dist/js/adminlte.min.js')}}"></script>
-<script type="text/javascript" src="{{ asset('modules/contact/js/app.js') }}"></script>
+@yield('js')
 </body>
 </html>
