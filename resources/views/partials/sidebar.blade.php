8e1c8034f4e9c5a4ad6ee36eed503f2255796e43
Rails <aluong343@gmail.com>
2020-11-30 23:54:51 +0700

    commit

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -55,7 +55,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    {{--                    <a href="{{route('users.index')}}" class="nav-link">--}}
+                    <a href="{{route('users.index')}}" class="nav-link">
                     <i class="nav-icon fas fa-th"></i>
                     <p>
                         User
