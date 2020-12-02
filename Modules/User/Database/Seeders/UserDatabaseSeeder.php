5c5705c63aad7f7672c8fd173a1a919923b689a8
Thanhhff <thanhnguyen200399@gmail.com>
2020-12-02 09:01:46 +0700

    Update Role User

--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -16,9 +16,9 @@ class UserDatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('roles')->insert([
-            ['name' => 'admin', 'display_name' => 'Quản trị hệ thống']
+            ['name' => 'admin', 'display_name' => 'Quản trị hệ thống'],
             ['name' => 'guest', 'display_name' => 'Khách hàng'],
-           ['name' => 'developer', 'display_name' => 'Phát triển hệ thống'],
+            ['name' => 'developer', 'display_name' => 'Phát triển hệ thống'],
             ['name' => 'content', 'display_name' => 'Chỉnh sửa nội dung'],
         ]);
     }
