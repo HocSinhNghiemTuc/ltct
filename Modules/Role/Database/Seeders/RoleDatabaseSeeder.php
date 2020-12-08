d8a2cba4bb7de8fec38f5aed5e8201e6e0133e71
Rails <aluong343@gmail.com>
2020-12-08 14:39:10 +0700

    Admin acc: example@gmail.com      12121999

--- Modules/Role/Database/Seeders/RoleDatabaseSeeder.php
+++ Modules/Role/Database/Seeders/RoleDatabaseSeeder.php
@@ -16,7 +16,13 @@ class RoleDatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            ['name' => 'admin', 'email' => 'example@gmail.com','password' => '$2y$10$.jWZ.STpWtmF04ImR6YCke91VDfecF5LwgRPP2HNyZrI5peBhabN.'],
+            ['id'=>'20','name' => 'admin', 'email' => 'example@gmail.com','password' => '$2y$10$.jWZ.STpWtmF04ImR6YCke91VDfecF5LwgRPP2HNyZrI5peBhabN.'],
+        ]);
+        DB::table('roles')->insert([
+            ['id' => '20', 'name' => 'admin', 'display_name' => 'Quan tri he thong'],
+        ]);
+        DB::table('role_user')->insert([
+            ['user_id' => '20','role_id' => '20'],
         ]);
     }
 }
