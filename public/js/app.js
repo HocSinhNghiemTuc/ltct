c3aa270af292394a4437a25e65eb921c18548ead
baoanh1310 <55654762+baoanh1310@users.noreply.github.com>
2020-12-29 12:41:25 +0700

    Change composer lock file

--- public/js/app.js
+++ public/js/app.js
@@ -49875,7 +49875,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\n\r\n@import '~@fortawesome/fontawesome-free/scss/fontawesome';\r\n^\r\n      File to import not found or unreadable: ~@fortawesome/fontawesome-free/scss/fontawesome.\r\n      in C:\\Users\\Toshiba T75\\Desktop\\Github\\ltct\\resources\\sass\\app.scss (line 9, column 1)\n    at C:\\Users\\Toshiba T75\\Desktop\\Github\\ltct\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Users\\Toshiba T75\\Desktop\\Github\\ltct\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\Toshiba T75\\Desktop\\Github\\ltct\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\Users\\Toshiba T75\\Desktop\\Github\\ltct\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.callback (C:\\Users\\Toshiba T75\\Desktop\\Github\\ltct\\node_modules\\sass-loader\\dist\\index.js:89:7)\n    at Object.done [as callback] (C:\\Users\\Toshiba T75\\Desktop\\Github\\ltct\\node_modules\\neo-async\\async.js:8069:18)\n    at options.error (C:\\Users\\Toshiba T75\\Desktop\\Github\\ltct\\node_modules\\node-sass\\lib\\index.js:294:32)");
 
 /***/ }),
 
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/hsnt/ドキュメント/Project/ltct/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/hsnt/ドキュメント/Project/ltct/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Toshiba T75\Desktop\Github\ltct\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Toshiba T75\Desktop\Github\ltct\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
